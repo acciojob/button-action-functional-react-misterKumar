48c7a629517e9eb69ab0821337ab9cae6647341a
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-23 08:21:57 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import React, {Component, useState} from "react";
+import React, {Component, useState,useEffect} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button
+        id="click"
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+      >
+        Click
+      </button>
+      {clicked && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
